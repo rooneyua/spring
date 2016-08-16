c46df643e73672cf7976ec6c07ac9a88c061826b
Yuriy Peresypkin <rooneyua@gmail.com>
2016-08-16 23:52:53 +0300

    added spring maven dependencies spring beans configuration added added PetShop bean

--- src/main/java/rooneyua/spring/App.java
+++ src/main/java/rooneyua/spring/App.java
@@ -1,13 +1,18 @@
 package rooneyua.spring;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"beans.xml"});
+		
+		PetShop petShop = context.getBean("petShop", PetShop.class);
+		
+		System.out.println(petShop.getName());
+	}
 }
---
+++ src/main/java/rooneyua/spring/PetShop.java
@@ -0,0 +1,16 @@
+package rooneyua.spring;
+
+public class PetShop {
+
+	private String name;
+
+	public PetShop() {}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
